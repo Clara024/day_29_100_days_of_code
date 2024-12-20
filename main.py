fd63a37fb2000e3e292251ba7d348adabf39c616
Izundu Clara Chinecherem <ci.chinecherem@stu.unizik.edu.ng>
2024-12-20 21:51:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-for i in range(0, 100):
-  print(i, end=" ")
+yellow = "\033[33m"
+blue = "\033[34m"
+red = "\033[31m"
+purple = "\033[35m"
+default = "\033[0m"
+
+print(yellow,"Super Subroutine","\n","With my ",blue,"new program",yellow," I can just call red",red," and", yellow," that word will appear in the colour it is set to","\n","with no",purple," weird gaps","\n","Epic",default,sep ="")
